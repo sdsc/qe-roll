ad50c244fa5e45ec49d02427016807f644da0907
Trevor Cooper <tcooper@sdsc.edu>
2013-11-27 14:04:00 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/qe/version.mk
+++ src/qe/version.mk
@@ -1,3 +1,14 @@
-NAME    = qe_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
-VERSION	= 5.0.1
-RELEASE	= 1
+NAME               = qe_$(ROLLCOMPILER)_$(ROLLMPI)_$(ROLLNETWORK)
+VERSION            = 5.0.1
+RELEASE            = 1
+PKGROOT            = /opt/qe
+
+SRC_SUBDIR         = qe
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
